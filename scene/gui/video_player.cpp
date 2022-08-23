1cdf5130f0eaed03aad1d7b3c4992203b0a74bb6
Michael Alexsander <michaelalexsander@protonmail.com>
2022-08-23 18:30:09 -0300

    Add method to get the length of a video stream

--- scene/gui/video_player.cpp
+++ scene/gui/video_player.cpp
@@ -359,6 +359,13 @@ void VideoPlayer::set_stream_position(float p_position) {
 	}
 }
 
+float VideoPlayer::get_stream_length() const {
+	if (playback.is_null()) {
+		return 0;
+	}
+	return playback->get_length();
+}
+
 Ref<Texture> VideoPlayer::get_video_texture() const {
 	if (playback.is_valid()) {
 		return playback->get_texture();
@@ -431,6 +438,8 @@ void VideoPlayer::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("set_stream_position", "position"), &VideoPlayer::set_stream_position);
 	ClassDB::bind_method(D_METHOD("get_stream_position"), &VideoPlayer::get_stream_position);
+
+	ClassDB::bind_method(D_METHOD("get_stream_length"), &VideoPlayer::get_stream_length);
 
 	ClassDB::bind_method(D_METHOD("set_autoplay", "enabled"), &VideoPlayer::set_autoplay);
 	ClassDB::bind_method(D_METHOD("has_autoplay"), &VideoPlayer::has_autoplay);
