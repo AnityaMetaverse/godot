d964e38f8c57a7cdc4c053a84e9abf0e6a7cfc5b
JM <jordan.j.motta.v@gmail.com>
2022-10-24 14:26:57 -0400

    Copied files from audio branch.

---
+++ modules/anitya_server/register_types.cpp
@@ -0,0 +1,9 @@
+#include "register_types.h"
+
+#include "core/class_db.h"
+
+void register_anitya_server_types() {
+    
+}
+
+void unregister_anitya_server_types() {}
---
+++ modules/anitya_server/register_types.h
@@ -0,0 +1,7 @@
+#ifndef FFMPEG_ANITYA_SERVER_TYPES_H
+#define FFMPEG_ANITYA_SERVER_TYPES_H
+
+void register_anitya_server_types();
+void unregister_anitya_server_types();
+
+#endif 
